a1d593ebfa1c2e68f0f664b4d23e717fa2ca1d54
Vojtěch Bartoš <barvoj@seznam.cz>
2016-05-01 01:10:40 +0200

    Next try

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ mkdir $BUILD_NAME
 mv * $BUILD_NAME
 tar -czf build.tgz $BUILD_NAME
 
-scp -p 11172 -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
+scp -P 11172 -o 'StrictHostKeyChecking no' build.tgz app@alpha-node-2.rosti.cz:/srv/
 ssh app@alpha-node-2.rosti.cz -p 11172 -o 'StrictHostKeyChecking no' "tar xfz /srv/build.tgz && /srv/${BUILD_NAME}/install.sh ${BUILD_NAME}"
