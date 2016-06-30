05ac93e785cdd07fdf52f77980542bfb864ea3f7
Vojtěch Bartoš <barvoj@seznam.cz>
2016-06-30 21:32:49 +0200

    Remove bower and gulp. Add webpack instead.

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,10 @@ FROM ubuntu
 MAINTAINER Vojtech Bartos <docker@vojtechbartos.com>
 
 COPY ./ /project
-COPY ./db /docker-entrypoint-initdb.d
 
 RUN chown -R www-data:www-data /project
 RUN chmod -R 777 /project
 
 VOLUME /project
-VOLUME /docker-entrypoint-initdb.d
 
 CMD ["true"]
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./resources/js/script.jsx",
+    output: {
+        path: "./www/js",
+        filename: "script.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader", "less-loader")
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader:"url?prefix=font/&limit=5000"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("../css/style.css")
+    ]
+};
