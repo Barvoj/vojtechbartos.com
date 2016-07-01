1cad4ddebefce591851cc7513c05451a412b924b
Vojtěch Bartoš <barvoj@seznam.cz>
2016-07-01 10:25:23 +0200

    Add missing templates

---
+++ extensions/Article/Components/ArticleList/template/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Article\Components\ArticleList\Template;
+
+use Libs\Application\UI\Template;
+
+/**
+ * @property $showLink
+ * @property $publishLink
+ * @property $editLink
+ * @property $articles
+ */
+class Main extends Template
+{
+
+}
---
+++ extensions/Article/Components/ArticleListItem/template/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Article\Components\ArticleListItem\Template;
+
+use Libs\Application\UI\Template;
+
+/**
+ * @property $showLink
+ * @property $publishLink
+ * @property $editLink
+ * @property $deleteLink
+ * @property $article
+ */
+class Main extends Template
+{
+}
